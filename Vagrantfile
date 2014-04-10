8f63eb637d48640fa3579bfcdaa7e2a0f93955a9
milenangelov <milen.angelov@bitmix.co.uk>
2014-04-10 15:55:24 +0300

    ver. 0.1

--- Vagrantfile
+++ Vagrantfile
@@ -1,13 +1,14 @@
 
 Vagrant::Config.run do |config|
   config.vm.box = "opscode-fedora-20"
-  config.vm.forward_port 80, 3000
+  config.vm.forward_port 80, 8084
   config.vm.host_name = "development.milenlab.vm"
   config.vm.network :hostonly, "192.168.33.10"
   
   config.vm.provision :puppet do |puppet|
     puppet.manifests_path = "manifests"
-    puppet.manifest_file  = "default.pp"
+    puppet.module_path	  = "modules"    
+    puppet.manifest_file  = "site.pp"
   end
 end
 
---
+++ manifests/site.pp
@@ -0,0 +1,24 @@
+node 'development.milenlab.vm' {
+
+  # Configure apache
+  include apache
+  include apache::mod::php
+  apache::vhost { $::fqdn:
+    port    => '80',
+    docroot => '/var/www/test',
+    require => File['/var/www/test'],
+  }
+
+  # Configure Docroot and index.html
+  file { ['/var/www', '/var/www/test']:
+    ensure => directory
+  }
+
+  file { '/var/www/test/index.php':
+    ensure  => file,
+    content => '<?php echo \'<p>Hello World</p>\'; ?> ',
+  }
+
+  # Realize the Firewall Rule
+  Firewall <||>
+}
