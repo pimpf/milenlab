af95c62bc979c309b5801c4c0c766b280a98e2e2
milenangelov <milen.angelov@bitmix.co.uk>
2014-04-11 11:16:51 +0300

    ver. 0.2ver. 0.2   Please enter the commit message for your changes. Lines starting

--- Vagrantfile
+++ Vagrantfile
@@ -1,14 +1,19 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
 
 Vagrant::Config.run do |config|
-  config.vm.box = "opscode-fedora-20"
+  config.vm.box = "ubuntu13.10"
+  config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/saucy/current/saucy-server-cloudimg-amd64-vagrant-disk1.box"
   config.vm.forward_port 80, 8084
-  config.vm.host_name = "development.milenlab.vm"
-  config.vm.network :hostonly, "192.168.33.10"
-  
-  config.vm.provision :puppet do |puppet|
-    puppet.manifests_path = "manifests"
-    puppet.module_path	  = "modules"    
-    puppet.manifest_file  = "site.pp"
-  end
+  config.vm.network "hostonly", "192.168.33.10"
+  config.vm.host_name = "node01.milenlab.com"
+
+#  config.vm.provision :puppet, :module_path => "modules"  
+
+#  config.vm.provision :puppet do |puppet|
+#    puppet.manifests_path = "manifests"   
+#    puppet.manifest_file  = "default.pp"
+#    puppet.options = "--verbose --debug"
+#  end
 end
 
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,17 +1,26 @@
-exec { "apt-get update":
-  path => "/usr/bin",
-}
-package { "apache2":
-  ensure  => present,
-  require => Exec["apt-get update"],
-}
-service { "apache2":
-  ensure  => "running",
-  require => Package["apache2"],
-}
-file { "/var/www/sample-webapp":
-  ensure  => "link",
-  target  => "/vagrant/sample-webapp",
-  require => Package["apache2"],
-  notify  => Service["apache2"],
+node 'node01.milenlab.vm' {
+  	group { "puppet":
+		ensure => present,
+  	}
+
+  	# to update outdated package list
+  	exec { "refresh-packages":    
+    	command => "/usr/bin/apt-get update",
+    	before => Package["apache2"],
+  	}
+
+  	package { "apache2":
+		ensure => installed,
+	}
+	  
+	service { "apache2":
+		ensure => running,
+		require => Package["apache2"],
+	}
+	  
+	file { "/var/www/index.html":
+		ensure => file,
+		content => "<html><body><h1>Vagrant and Puppet test!</h1></body></html>",
+		require => Service["apache2"],
+	}	
 }
